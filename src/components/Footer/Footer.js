cef068774a687a9b43b2ad016493b86edec12445
BlueButterflies <dsvk23020818@outlook.it>
2024-01-16 13:19:45 +0100

    add faStackOverflow icon

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Container from "react-bootstrap/Container";
-import { FaGithub, FaLinkedinIn, FaCopyright } from "react-icons/fa";
+import { FaGithub, FaLinkedinIn, FaStackOverflow, FaCopyright } from "react-icons/fa";
 
 function Footer() {
     return (
@@ -12,7 +12,9 @@ function Footer() {
                 <FaLinkedinIn className="icon ma3" />
             </a>
 
-
+            <a href="https://stackexchange.com/users/30342116/siyana-zdravkova" target="_blank">
+                <FaStackOverflow className="icon ma3" />
+            </a>
             <hr />
 
             <h6 className="mt3">
