da542a8398d06f0d6ee174c496f9e683d4e82594
BlueButterflies <dsvk23020818@outlook.it>
2024-01-16 12:41:16 +0100

    add correct url section footer copyright

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -17,7 +17,7 @@ function Footer() {
 
             <h6 className="mt3">
                 <FaCopyright className="copy" />
-                <a href="https://github.com/BlueButterflies/personalportfolio" target="_blank">
+                <a href="https://github.com/BlueButterflies?tab=repositories" target="_blank">
                     {"2024 - S/Z Siyana Zdravkova Web Developer"}
                 </a>
             </h6>
