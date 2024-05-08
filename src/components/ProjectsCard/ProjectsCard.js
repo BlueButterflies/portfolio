8779eaf00afda201cf58e47af23136705232e50b
BlueButterflies <dsvk23020818@outlook.it>
2024-05-08 15:20:30 +0200

    change buttons position project section

--- src/components/ProjectsCard/ProjectsCard.js
+++ src/components/ProjectsCard/ProjectsCard.js
@@ -7,7 +7,7 @@ const ProjectsCard = () => {
         <div className="projects">
             <h1 className="tc navy">Projects</h1>
             <div class="container d-flex align-items-center justify-content-center position-relative flex-wrap">
-                
+
                 {
                     Data.map((data) => {
                         if (data.isWebsite) {
@@ -19,20 +19,21 @@ const ProjectsCard = () => {
                                     <div class="content navy">
                                         <h2>{data.title}</h2>
                                         <p>{data.description}</p>
-                                        </div>
                                         <div className="tc btns">
-                                        <a href={data.urlBtnLive} target="_blank" className="btnLive f3 tc no-underline inline-flex items-center ba br4 pointer">
-                                            <FaEye className="btnLiveIcon" />
-                                        </a>
-                                        <a href={data.urlBtnCode} target="_blank" className="btnSource f3 tc no-underline inline-flex items-center ba br4 pointer">
-                                            <FaCode className="btnSourceIcon" />
-                                        </a>
+                                            <a href={data.urlBtnLive} target="_blank" className="btnLive f3 tc no-underline inline-flex items-center ba br4 pointer">
+                                                <FaEye className="btnLiveIcon" />
+                                            </a>
+                                            <a href={data.urlBtnCode} target="_blank" className="btnSource f3 tc no-underline inline-flex items-center ba br4 pointer">
+                                                <FaCode className="btnSourceIcon" />
+                                            </a>
+                                        </div>
                                     </div>
-                                    
+
+
                                 </div>
                             )
-                        }else if(data.isAndroid && data.isWindows){
-                            return(
+                        } else if (data.isAndroid && data.isWindows) {
+                            return (
                                 <div class="card d-flex position-relative flex-column">
                                     <div class='imgContainer'>
                                         <img src={data.image} className="img" />
@@ -40,23 +41,24 @@ const ProjectsCard = () => {
                                     <div class="content navy">
                                         <h2>{data.title}</h2>
                                         <p>{data.description}</p>
+                                        <div className="tc btns">
+                                            <a href={data.urlBtnWindows} target="_blank" className="btnWindows f3 tc no-underline inline-flex items-center ba br4 pointer">
+                                                <FaWindows className="btnWindowsIcon" />
+                                            </a>
+                                            <a href={data.urlBtnAndroid} target="_blank" className="btnAndroid f3 tc no-underline inline-flex items-center ba br4 pointer">
+                                                <FaAndroid className="btnAndroidIcon" />
+                                            </a>
+                                            <a href={data.urlBtnCode} target="_blank" className="btnSource f3 tc no-underline inline-flex items-center ba br4 pointer">
+                                                <FaCode className="btnSourceIcon" />
+                                            </a>
+                                        </div>
                                     </div>
 
-                                    <div className="tc btns">
-                                        <a href={data.urlBtnWindows} target="_blank" className="btnWindows f3 tc no-underline inline-flex items-center ba br4 pointer">
-                                            <FaWindows className="btnWindowsIcon" />
-                                        </a>
-                                        <a href={data.urlBtnAndroid} target="_blank" className="btnAndroid f3 tc no-underline inline-flex items-center ba br4 pointer">
-                                            <FaAndroid className="btnAndroidIcon" />
-                                        </a>
-                                        <a href={data.urlBtnCode} target="_blank" className="btnSource f3 tc no-underline inline-flex items-center ba br4 pointer">
-                                            <FaCode className="btnSourceIcon" />
-                                        </a>
-                                    </div>
+
                                 </div>
                             )
-                        }else if(data.isWindows){
-                            return(
+                        } else if (data.isWindows) {
+                            return (
                                 <div class="card d-flex position-relative flex-column">
                                     <div class='imgContainer'>
                                         <img src={data.image} className="img" />
@@ -64,21 +66,20 @@ const ProjectsCard = () => {
                                     <div class="content navy">
                                         <h2>{data.title}</h2>
                                         <p>{data.description}</p>
-                                    </div>
-
-                                    <div className="tc btns">
-                                        <a href={data.urlBtnWindows} target="_blank" className="btnWindows f3 tc no-underline inline-flex items-center ba br4 pointer">
-                                            <FaWindows className="btnWindowsIcon" />
-                                        </a>
-                                        <a href={data.urlBtnCode} target="_blank" className="btnSource f3 tc no-underline inline-flex items-center ba br4 pointer">
-                                            <FaCode className="btnSourceIcon" />
-                                        </a>
+                                        <div className="tc btns">
+                                            <a href={data.urlBtnWindows} target="_blank" className="btnWindows f3 tc no-underline inline-flex items-center ba br4 pointer">
+                                                <FaWindows className="btnWindowsIcon" />
+                                            </a>
+                                            <a href={data.urlBtnCode} target="_blank" className="btnSource f3 tc no-underline inline-flex items-center ba br4 pointer">
+                                                <FaCode className="btnSourceIcon" />
+                                            </a>
+                                        </div>
                                     </div>
                                 </div>
                             )
 
-                        }else if(data.isAndroid){
-                            return(
+                        } else if (data.isAndroid) {
+                            return (
                                 <div class="card d-flex position-relative flex-column" >
                                     <div class='imgContainer'>
                                         <img src={data.image} className="img" />
@@ -86,109 +87,24 @@ const ProjectsCard = () => {
                                     <div class="content navy">
                                         <h2>{data.title}</h2>
                                         <p>{data.description}</p>
+                                        <div className="tc btns">
+                                            <a href={data.urlBtnWindows} target="_blank" className="btnAndroid f3 tc no-underline inline-flex items-center ba br4 pointer">
+                                                <FaAndroid className="btnAndroidIcon" />
+                                            </a>
+                                            <a href={data.urlBtnCode} target="_blank" className="btnSource f3 tc no-underline inline-flex items-center ba br4 pointer">
+                                                <FaCode className="btnSourceIcon" />
+                                            </a>
+                                        </div>
                                     </div>
 
-                                    <div className="tc btns">
-                                        <a href={data.urlBtnWindows} target="_blank" className="btnAndroid f3 tc no-underline inline-flex items-center ba br4 pointer">
-                                            <FaAndroid className="btnAndroidIcon" />
-                                        </a>
-                                        <a href={data.urlBtnCode} target="_blank" className="btnSource f3 tc no-underline inline-flex items-center ba br4 pointer">
-                                            <FaCode className="btnSourceIcon" />
-                                        </a>
-                                    </div>
+
                                 </div>
                             )
                         }
-                                            
+
                     })
                 }
             </div>
-            {/* <div className="cross">
-                <Carousel data-bs-theme="dark" className="carousel">
-                {
-                    Data.map((data) => {
-                        if (data.isWebsite) {
-                            return (
-                                <Carousel.Item>
-                                    <Image
-                                        rounded
-                                        className="images"
-                                        src={bird}
-                                        alt="websites"
-                                    />
-                                    <div className="tc btns">
-                                        <a href={data.urlBtnLive} target="_blank" className="btnLive f3 tc no-underline inline-flex items-center ba br4 pointer">
-                                            <FaEye className="btnLiveIcon" /><b>Live</b>
-                                        </a>
-                                        <a href={data.urlBtnCode} target="_blank" className="btnSource f3 tc no-underline inline-flex items-center ba br4 pointer">
-                                            <FaCode className="btnSourceIcon" /><b>Source</b>
-                                        </a>
-                                    </div>
-                                </Carousel.Item>
-                            )
-                        } else if (data.isAndroid && data.isWindows) {
-                            return (
-                                <Carousel.Item>
-                                    <img
-                                        className="images d-block "
-                                        src={data.image}
-                                        alt="gameApp"
-                                    />
-                                    <div className="tc btns">
-                                        <a href={data.urlBtnWindows} target="_blank" className="btnWindows f3 tc no-underline inline-flex items-center ba br4 pointer">
-                                            <FaWindows className="btnWindowsIcon" /><b><FaDownload className="download" /></b>
-                                        </a>
-                                        <a href={data.urlBtnAndroid} target="_blank" className="btnAndroid f3 tc no-underline inline-flex items-center ba br4 pointer">
-                                            <FaAndroid className="btnAndroidIcon" /><b><FaDownload className="download" /></b>
-                                        </a>
-                                        <a href={data.urlBtnCode} target="_blank" className="btnSource f3 tc no-underline inline-flex items-center ba br4 pointer">
-                                            <FaCode className="btnSourceIcon" /><b>Source</b>
-                                        </a>
-                                    </div>
-                                </Carousel.Item>
-                            )
-                        } else if (data.isWindows) {
-                            return (
-                                <Carousel.Item>
-                                    <img
-                                        className="images d-block"
-                                        src={data.image}
-                                        alt="desktopApp"
-                                    />
-                                    <div className="tc btns">
-                                        <a href={data.urlBtnWindows} target="_blank" className="btnWindows f3 tc no-underline inline-flex items-center ba br4 pointer">
-                                            <FaWindows className="btnWindowsIcon" /><b><FaDownload className="download" /></b>
-                                        </a>
-                                        <a href={data.urlBtnCode} target="_blank" className="btnSource f3 tc no-underline inline-flex items-center ba br4 pointer">
-                                            <FaCode className="btnSourceIcon" /><b>Source</b>
-                                        </a>
-                                    </div>
-                                </Carousel.Item>
-                            )
-                        } else if (data.isAndroid) {
-                            return (
-                                <Carousel.Item>
-                                    <img
-                                        className="images d-block"
-                                        src={data.image}
-                                        alt="androidApp"
-                                    />
-                                    <div className="tc btns">
-                                        <a href={data.urlBtnWindows} target="_blank" className="btnAndroid f3 tc no-underline inline-flex items-center ba br4 pointer">
-                                            <FaAndroid className="btnAndroidIcon" /><b><FaDownload className="download" /></b>
-                                        </a>
-                                        <a href={data.urlBtnCode} target="_blank" className="btnSource f3 tc no-underline inline-flex items-center ba br4 pointer">
-                                            <FaCode className="btnSourceIcon" /><b>Source</b>
-                                        </a>
-                                    </div>
-                                </Carousel.Item>
-                            )
-                        }
-                    })
-                }
-            </Carousel>
-            </div> */}
-
         </div>
     );
 }
