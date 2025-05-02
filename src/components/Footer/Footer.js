b37305638e15978bcc1376edf10d4c38d143d9a5
BlueButterflies <dsvk23020818@outlook.it>
2025-05-02 18:43:44 +0200

    change footer section

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,24 +1,14 @@
 import React from "react";
 import Container from "react-bootstrap/Container";
-import { FaGithub, FaStackOverflow, FaCopyright } from "react-icons/fa";
+import { FaCopyright, BsHeartHalf } from "react-icons/fa";
 
 function Footer() {
     return (
         <Container className="footer tc">
-            <a href="https://github.com/BlueButterflies" target="_blank"rel="noreferrer">
-                <FaGithub className="icon ma3" />
-            </a>
-
-            <a href="https://stackexchange.com/users/30342116/siyana-zdravkova" target="_blank" rel="noreferrer">
-                <FaStackOverflow className="icon ma3" />
-            </a>
-            
-            <hr />
-
             <h6 className="mt3">
                 <FaCopyright className="copy" />
                 <a href="https://github.com/BlueButterflies?tab=repositories" target="_blank" rel="noreferrer">
-                    2024 - <span>S/Z Siyana Zdravkova Web Developer</span>
+                    2024 - Create with <BsHeartHalf className="copy" /> by<span>S/Z Siyana Zdravkova Web Developer</span>
                 </a>
             </h6>
         </Container>
